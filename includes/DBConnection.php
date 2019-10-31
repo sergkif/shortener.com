170f931a61c1abe5b0f4fb8f2bc7b2609541f819
SergKif <sergkif2013@gmail.com>
2019-10-31 13:37:26 +0300

    Tryin to load it on heroku with ClearDB

--- includes/DBConnection.php
+++ includes/DBConnection.php
@@ -1,7 +1,43 @@
 <?php
 
-$connection = mysqli_connect('127.0.0.1', 'mysql', 'mysql', 'shorturl_db');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$connection = new mysqli($server, $username, $password, $db);
 
 if($connection == false) {
-    die("Connection error!<br>" . $connection->error());
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$checkShort = $connection->query('SELECT * FROM `short` LIMIT 1');
+$checkUsers = $connection->query('SELECT * FROM `users` LIMIT 1');
+
+$sqlShort = "CREATE TABLE `short` (
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `long_URL` text NOT NULL,
+  `user_IP` text NOT NULL,
+  `date` datetime NOT NULL
+)";
+
+$sqlUsers = "CREATE TABLE `users` (
+  `login` text UNIQUE NOT NULL,
+  `password` text NOT NULL,
+  `admin` tinyint(1) NOT NULL
+)";
+
+
+if(!$checkShort) {
+    if (!($connection->query($sqlShort) === TRUE)) {
+        die("Error creating table: " . $connection->error);
+    }
+}
+
+if(!$checkUsers) {
+    if (!($connection->query($sqlUsers) === TRUE)) {
+        die("Error creating table: " . $connection->error);
+    }
 }
